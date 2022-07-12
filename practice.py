ea4ddde39c39a65e1b06ed12c03dba1b98c4f6d1
Beatriceanena <anenabeattrice@gmail.com>
2022-07-12 11:00:34 +0300

    changes

---
+++ practice.py
@@ -0,0 +1,14 @@
+x=range(0,100)
+for y in x:
+    if y%5==0:
+       print(y)
+    elif y%6==0:
+        print(y)
+    elif y%7==0:
+        print(y)
+    elif y%8==0:
+        print(y)
+    else:
+
+
+      
